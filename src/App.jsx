9a91d5d55c2f8443c7aa23af8235686d2361eddf
Michael Red'kov <michael1redkov@gmail.com>
2026-03-20 20:40:53 +0300

    initialize...

---
+++ src/App.jsx
@@ -0,0 +1,85 @@
+import './App.css';
+import {useState, useEffect, useMemo, useRef} from 'react'
+import betrayalSound from './assets/Betrayal_.mp3';
+const formatTime = (seconds) => {
+    const mins = Math.floor(seconds / 60);
+    const secs = seconds % 60;
+
+    return `${mins.toString().padStart(2, '0')}:${secs.toString().padStart(2, '0')}`;
+};
+
+export default function App() {
+
+    const [time, setTime] = useState(0)
+    const [start, setStart] = useState(false)
+    const [limit, setLimit] = useState(1)
+
+    const audioRef = useRef(new Audio(betrayalSound));
+
+    const toggle = () => {
+        // ВАЖНО: Браузер разрешит звук, если вызвать .play() или .load()
+        // внутри обработчика клика (onClick)
+        audioRef.current.load();
+        setStart(!start);
+    };
+
+    const set = () => {
+        setStart(!start)
+    }
+
+    const clear = () => {
+        setTime((prev) => prev = 0)
+    }
+
+    useEffect(() => {
+
+        if(start) {
+            const interval = setInterval(() => {
+                setTime((prev) => {
+                    if (limit > 0 && prev >= limit * 60) {
+                        setStart(false)
+                        return prev
+                    }
+                    return prev + 1
+                })
+            }, 1000)
+            return () => clearInterval(interval)
+        }
+
+    }, [start , limit])
+
+    useEffect(() => {
+        if (limit > 0 && time >= limit * 60) {
+            setStart(false);
+            audioRef.current.play().catch(err => console.error("Ошибка:", err));
+        }
+    }, [time, limit]);
+
+    return (
+        <div className="app">
+            <h1>Timer</h1>
+            <h1>{formatTime(time)}</h1>
+            <div className="timer-info">
+                {start ?
+                    <span className="status running">● Running</span>
+                    : <span className="status stopped">● Stopped</span>
+                }
+            </div>
+
+            <div className="menu">
+                <button onClick={set}
+                        className={start ? 'stop' : 'start'}
+                >{start ? 'stop' : 'start'}
+                </button>
+                <button onClick={clear}
+                        className="clear">Clear
+                </button>
+            </div>
+            <input type="number"
+                   value={limit}
+                   onChange={(e) => setLimit(Number(e.target.value))}
+                   placeholder="minutes"
+            />
+        </div>
+    );
+}
